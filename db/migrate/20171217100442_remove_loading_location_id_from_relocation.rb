eb01f965792523410a1205b4957411b82d783079
S-COULIBALY <s.coulibaly@outlook.com>
2017-12-22 18:32:50 +0100

    ajout d'une migration initiale

--- db/migrate/20171217100442_remove_loading_location_id_from_relocation.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLoadingLocationIdFromRelocation < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :relocation, :loading_location_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171217100442) do
+ActiveRecord::Schema.define(version: 20171217095957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20171217100442) do
     t.float "volume"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "loading_location_id"
   end
 
   create_table "users", force: :cascade do |t|
