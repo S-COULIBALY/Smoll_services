72957dc4be9e11f4e293ddd6c81ce7a45745326f
S-COULIBALY <s.coulibaly@outlook.com>
2018-11-13 14:24:37 +0100

    Validation des attributs booléens (elevator & furniture_elevator) par l'inclusion toujours en échec

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,15 @@
 class Address < ApplicationRecord
   enum address_type: [:departure, :arrival]
 
-  validates :address_type, :complete_address, :carrying_distance, :floor_number, :elevator_capacity, :furniture_elevator,  presence: true
+  validates :address_type, :complete_address, :carrying_distance, :floor_number, :elevator_capacity,  presence: true
+
+  #validates :elevator, inclusion: {in: [true, false],
+  #message: "%{value} is not a valid elevator "}
+  #validates :furniture_elevator, inclusion: {in: [true, false],
+  #message: "%{value} is not a valid furniture_elevator "}
+
   validates :street_number, numericality: { greater_than_or_equal_to: 1 }
+
   validates :address_type, inclusion: { in: %w(departure arrival),
     message: "%{value} is not a valid address_type" }
   #validates :complete_address
