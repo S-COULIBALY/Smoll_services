1aa09ecdaf9238ba1f855050af5057fcef6f6fa9
Emmanuel Konzi <maverickkonzinov@gmail.com>
2020-08-18 23:30:49 +0200

    Raz des fichiers

--- app/models/relocation.rb
+++
@@ -1,6 +0,0 @@
-class Relocation < ApplicationRecord
-  belongs_to :departure, class_name: "Location", foreign_key: "departure_id"
-  belongs_to :arrival, class_name: "Location", foreign_key: "arrival_id"
-  accepts_nested_attributes_for :departure, allow_destroy: true
-  accepts_nested_attributes_for :arrival, allow_destroy: true
-end
--- db/migrate/20180119142209_add_departure_and_arrival_logic_to_relocation.rb
+++
@@ -1,6 +0,0 @@
-class AddDepartureAndArrivalLogicToRelocation < ActiveRecord::Migration[5.1]
-  def change
-     add_reference :relocations, :departure, foreign_key: {to_table: :locations}
-     add_reference :relocations, :arrival, foreign_key: {to_table: :locations}
-  end
-end
